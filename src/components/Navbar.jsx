9782656f8fca4ef01a0de59191a3b75d172dcb0b
alihaider0018 <ah18contact@gmail.com>
2024-08-19 14:04:42 +0530

    add changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="fixed top-0 z-10">
       <div className="w-screen flex justify-end items-center px-3 py-2 bg-slate-50 shadow font-bold text-xl gap-6">
         <Link to={"/week7-assignment"}>
-          <button className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 text-white rounded-md px-3 py-1 hover:cursor-pointer mr-9">
+          <button className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 text-white rounded-md px-3 py-1 hover:cursor-pointer ml-9">
             Home
           </button>
         </Link>
